b8c746bb174f7d175962b1db6b0f784ea1542ef4
Michal <m.j.olkiewicz@gmail.com>
2019-01-07 13:17:46 +0100

    Usunolem zipa i rozpokowalem go.

---
+++ WebApplication1/Models/Feedback.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Feedback
+    {
+        [BindNever]
+        public int FeedbackId { get; set; }
+        [Required]
+        [StringLength(100,ErrorMessage ="Your name is required")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Your email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(1000, ErrorMessage = "Your message is required")]
+        public string Message { get; set; }
+        public bool ContactMe { get; set; }
+
+
+    }
+}
