a6b348d91c0423e9c216f9413f3975189e071173
bialaporcelana <bialaporcelana@gmail.com>
2019-01-11 09:19:13 +0100

    dziala usuwanie i edytowanie ale na razie kazdy zalogowany to moze zrobic

---
+++ WebApplication1/WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+       
+
+        private UserManager<IdentityUser> _userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserManagement()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(AddUserViewModel addUserViewModel)
+        {
+            if (!ModelState.IsValid) return View(addUserViewModel);
+
+            var user = new IdentityUser()
+            {
+                UserName = addUserViewModel.UserName,
+                Email = addUserViewModel.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, addUserViewModel.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("UserManagement", _userManager.Users);
+            }
+
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(addUserViewModel);
+        }
+
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+                return RedirectToAction("UserManagement", _userManager.Users);
+
+            var claims = await _userManager.GetClaimsAsync(user);
+            var vm = new EditUserViewModel() { Id = user.Id, Email = user.Email, UserName = user.UserName };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel editUserViewModel)
+        {
+            var user = await _userManager.FindByIdAsync(editUserViewModel.Id);
+
+            if (user != null)
+            {
+                user.Email = editUserViewModel.Email;
+                user.UserName = editUserViewModel.UserName;
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                    return RedirectToAction("UserManagement", _userManager.Users);
+
+                ModelState.AddModelError("", "User not updated, something went wrong.");
+
+                return View(editUserViewModel);
+            }
+
+            return RedirectToAction("UserManagement", _userManager.Users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("UserManagement");
+                else
+                    ModelState.AddModelError("", "Something went wrong while deleting this user.");
+            }
+            else
+            {
+                ModelState.AddModelError("", "This user can't be found");
+            }
+            return View("UserManagement", _userManager.Users);
+        }
+
+    }
+   
+}
