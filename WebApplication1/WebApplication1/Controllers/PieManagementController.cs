a9a09cc0036e2cd00f3820837522b03d34427b95
bialaporcelana <bialaporcelana@gmail.com>
2019-01-14 15:01:17 +0100

    nie dziala edycja i usuwanie

---
+++ WebApplication1/WebApplication1/Controllers/PieManagementController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PieManagementController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieManagementController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.Pies.OrderBy(p => p.Id);
+            return View(pies);
+        }
+
+        public IActionResult AddPie()
+        {
+            var categories = _categoryRepository.Categories;
+            var pieEditViewModel = new PieEditViewModel
+            {
+                Categories = categories.Select(c => new SelectListItem() { Text = c.CategoryName, Value = c.CategoryId.ToString() }).ToList(),
+                CategoryId = categories.FirstOrDefault().CategoryId
+            };
+            return View(pieEditViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddPie(PieEditViewModel pieEditViewModel)
+        {
+            if (pieEditViewModel.Pie.Price < 0)
+                ModelState.AddModelError(nameof(pieEditViewModel.Pie.Price), "The price of the pie should be higher than 0");
+
+            
+            if (ModelState.IsValid)
+            {
+                pieEditViewModel.Pie.CategoryId = pieEditViewModel.CategoryId;
+                _pieRepository.CreatePie(pieEditViewModel.Pie);
+                return RedirectToAction("Index");
+            }
+
+            return View(pieEditViewModel);
+        }
+
+
+        public IActionResult EditPie([FromQuery]int id)
+        {
+            var categories = _categoryRepository.Categories;
+
+            var pie = _pieRepository.Pies.FirstOrDefault(p => p.Id == id);
+
+            var pieEditViewModel = new PieEditViewModel
+            {
+                Categories = categories.Select(c => new SelectListItem() { Text = c.CategoryName, Value = c.CategoryId.ToString() }).ToList(),
+                Pie = pie,
+                CategoryId = pie.CategoryId
+            };
+
+            var item = pieEditViewModel.Categories.FirstOrDefault(c => c.Value == pie.CategoryId.ToString());
+            item.Selected = true;
+
+            return View(pieEditViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult EditPie(PieEditViewModel pieEditViewModel)
+        {
+            pieEditViewModel.Pie.CategoryId = pieEditViewModel.CategoryId;
+
+            if (ModelState.IsValid)
+            {
+                _pieRepository.UpdatePie(pieEditViewModel.Pie);
+                return RedirectToAction("Index");
+            }
+            return View(pieEditViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePie([FromQuery]int Id)
+        {
+            var pie = _pieRepository.GetPieById(Id);
+            _pieRepository.DeletePie(pie);
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IEnumerable<Category> Categories => _appDbContext.Categories;
+
+        public Category GetCategoryById(int categoryId)
+        {
+            return _appDbContext.Categories.FirstOrDefault(p => p.CategoryId == categoryId);
+        }
+    }
+}
