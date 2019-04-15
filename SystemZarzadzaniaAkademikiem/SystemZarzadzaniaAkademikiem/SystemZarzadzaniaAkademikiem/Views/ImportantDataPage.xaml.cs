8991704699db4098117359434c4604e4482ed4eb
Magda <bialaporcelana@gmail.com>
2019-04-15 21:09:00 +0200

    dodanie odwolania do userViewModel

--- SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/Views/ImportantDataPage.xaml.cs
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/Views/ImportantDataPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SystemZarzadzaniaAkademikiem.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +12,12 @@ namespace SystemZarzadzaniaAkademikiem.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ImportantDataPage : ContentPage
 	{
-		public ImportantDataPage ()
+        private UserViewModel userViewModel;
+        public ImportantDataPage ()
 		{
 			InitializeComponent ();
-		}
+            userViewModel = new UserViewModel();
+        }
 
         private async void Button_ClickedAsync(object sender, EventArgs e)
         {
