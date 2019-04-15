6cf2290c42c84f92ac0c68cafdb6b0fcf4ba7612
Magda <bialaporcelana@gmail.com>
2019-04-15 21:06:35 +0200

    base walidator do spr czy pole niepuste i czy indeks ma 6 cyfr

---
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/Validators/Validator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SystemZarzadzaniaAkademikiem.Validators
+{
+    class Validator
+    {
+        public static bool EmptyField(string field)
+        {
+            if (field == null)
+                return false;
+            var str = field as string;
+            return !string.IsNullOrWhiteSpace(str);
+        }
+
+        public static bool IndexVal(string index)
+        {
+            if (index == null)
+                return false;
+            var tmp = index as string;
+            Regex regex = new Regex(@"^([1-9][0-9]{5})$");
+            Match match = regex.Match(tmp);
+            return match.Success;
+        }
+    }
+}
