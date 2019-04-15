992490c079c63ae4fbd38a4d8e2209d9df8d9f28
Magda <bialaporcelana@gmail.com>
2019-04-15 19:55:59 +0200

    dodane pola usera

--- SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/Models/User.cs
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/Models/User.cs
@@ -7,9 +7,22 @@ namespace SystemZarzadzaniaAkademikiem.Models
     class User
     {
         public int Id;
-        public string Imie { set; get; }
-        public string Nazwisko { set; get; }
+        public string Name { set; get; }
+        public string Lastname { set; get; }
         public string Indeks { set; get; }
-        public string Plec { set; get; }
+        public string Sex { set; get; }
+        public int Floor { set; get; }
+        public string BedLocation { set; get; }
+        public string SleepTime { set; get; }
+        public string WakeUpTime { set; get; }
+        public string HotOrNot { set; get; }
+        public string Music { set; get; }
+        public bool CleanUp { set; get; }
+        public string Talkative { set; get; }
+        public string StudyField { set; get; }
+        public bool Sporting { set; get; }
+        public string HomeBack { set; get; }
+        public bool Smoking { set; get; }
+        public string Party { set; get; }
     }
 }
