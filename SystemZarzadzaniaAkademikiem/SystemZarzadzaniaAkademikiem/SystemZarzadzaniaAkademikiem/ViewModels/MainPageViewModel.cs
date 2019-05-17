cf213ed9ba260e54ff00245eb93f17f584b38e1c
Michał <m.j.olkiewicz@gmail.com>
2019-05-17 18:55:54 +0200

    Dodalem sposob dostania sie do okna logowania admina.

--- SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/MainPageViewModel.cs
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/MainPageViewModel.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using SystemZarzadzaniaAkademikiem.Views;
+using Xamarin.Forms;
 
 namespace SystemZarzadzaniaAkademikiem.ViewModels
 {
     class MainPageViewModel : BaseViewModel
     {
+        public Command ActivateAdminLogin { get; set; }
+        public int Counter=0;
         public MainPageViewModel()
         {
             Title = "System przydzielania miejsc w akademiku";
+            ActivateAdminLogin = new Command(ExecuteActivateAdminLogin);
         }
+        async void ExecuteActivateAdminLogin()
+        {
+            if (Counter >= 7)
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new AdminLoginPage());
+            }
+            Counter++;
+            Debug.WriteLine(Counter);
+        }
+
     }
 }
--- SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/Views/MainPage.xaml.cs
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/Views/MainPage.xaml.cs
@@ -1,25 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
 using SystemZarzadzaniaAkademikiem.ViewModels;
+using Xamarin.Forms;
 
 namespace SystemZarzadzaniaAkademikiem.Views
 {
     public partial class MainPage : ContentPage
     {
-        MainDebugViewModel viewModel;
+        MainPageViewModel viewModel;
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = viewModel = new MainDebugViewModel();
+            BindingContext = viewModel = new MainPageViewModel();
         }
 
         async void Button_ClickedAsync(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ImportantDataPage());
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            viewModel.Counter = 0;
         }
     }
 }
