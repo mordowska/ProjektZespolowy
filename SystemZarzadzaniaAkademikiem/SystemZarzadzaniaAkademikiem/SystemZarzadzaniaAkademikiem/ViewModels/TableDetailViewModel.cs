99a2059a97cdb2740653747c7a5b28d4458af345
Michał <m.j.olkiewicz@gmail.com>
2019-06-01 16:26:42 +0200

    Poczatek usuwania

--- SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/TableDetailViewModel.cs
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/TableDetailViewModel.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SystemZarzadzaniaAkademikiem.ViewModels
 {
     public class TableDetailViewModel
     {
         public string name;
+        //public Command DeleteRecordCommand;
         public TableDetailViewModel(string name)
         {
             this.name = name;
+            //DeleteRecordCommand = new Command((id) => ExecuteDeleteRecordCommand());
         }
+        public Command DeleteRecordCommand
+        {
+            get
+            {
+                return new Command((id) =>
+                {
+                    Debug.WriteLine(id);
+
+                });
+            }
+        }
+        /*void ExecuteDeleteRecordCommand()
+        {
+            Debug.WriteLine(id);
+        }*/
     }
 }
