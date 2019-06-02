201cf9b1ec03c0b6f57e5c5aca09112f0582e297
Magda <bialaporcelana@gmail.com>
2019-06-02 19:43:25 +0200

    walidacja spec 1

--- SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/SpecificDataPage1ViewModel.cs
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/SpecificDataPage1ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using SystemZarzadzaniaAkademikiem.Models;
 using SystemZarzadzaniaAkademikiem.Services;
+using SystemZarzadzaniaAkademikiem.Validators;
 using Xamarin.Forms;
 
 namespace SystemZarzadzaniaAkademikiem.ViewModels
@@ -9,42 +10,235 @@ namespace SystemZarzadzaniaAkademikiem.ViewModels
         public readonly string index;
         private readonly User user;
         private readonly UserRepo userRepo;
+        public bool isValid = false;
+        private string _bedLocation = "";
+        private string _bedLocationError = "";
+        private string _sleepTime = "";
+        private string _sleepTimeError = "";
+        private string _wakeUpTime = "";
+        private string _wakeUpTimeError = "";
+        private string _hotOrNot = "";
+        private string _hotOrNotError = "";
+        private string _music = "";
+        private string _musicError = "";
+        private string _cleanUp = "";
+        private string _cleanUpError = "";
+        private string _floor = "";
+        private string _floorError = "";
 
         public SpecificDataPage1ViewModel(string index)
         {
             user = new User();
-            //Floor = user?.Floor;
-            //BedLocation = user?.BedLocation;
-            //CleanUp = user?.CleanUp;
-            //HotOrNot = user?.HotOrNot;
-            //Music = user?.Music;
-            //SleepTime = user?.SleepTime;
             this.index = index;
             SaveSpecificData1 = new Command(ExecuteSaveSpecificData1);
             userRepo = new UserRepo(App.Database);
         }
 
-        public string BedLocation { get; set; }
-        public string CleanUp { get; set; }
-        public string Floor { get; set; }
-        public string HotOrNot { get; set; }
-        public string Music { get; set; }
-        public string SleepTime { get; set; }
-        public string WakeUpTime { get; set; }
-
         public Command SaveSpecificData1 { get; set; }
 
         private async void ExecuteSaveSpecificData1()
         {
-            var user = userRepo.GetUserAsync(index).Result;
-            user.Floor = Floor;
-            user.BedLocation = BedLocation;
-            user.SleepTime = SleepTime;
-            user.WakeUpTime = WakeUpTime;
-            user.HotOrNot = HotOrNot;
-            user.Music = Music;
-            user.CleanUp = CleanUp;
-            await userRepo.SaveUserAsync(user);
+            isValid = Validate();
+            if (isValid)
+            {
+                var user = userRepo.GetUserAsync(index).Result;
+                user.Floor = Floor;
+                user.BedLocation = BedLocation;
+                user.SleepTime = SleepTime;
+                user.WakeUpTime = WakeUpTime;
+                user.HotOrNot = HotOrNot;
+                user.Music = Music;
+                user.CleanUp = CleanUp;
+                await userRepo.SaveUserAsync(user);
+            }
         }
+
+        public bool Validate()
+        {
+            return false;
+        }
+        #region
+        public string BedLocation
+        {
+            get => _bedLocation;
+            set
+            {
+                _bedLocation = value;
+                OnPropertyChanged();
+                BedLocationError = !ValidateBedLocation() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateBedLocation()
+        {
+            return Validator.EmptyField(BedLocation);
+        }
+
+        public string BedLocationError
+        {
+            get => _bedLocationError;
+            set
+            {
+                _bedLocationError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string CleanUp
+        {
+            get => _cleanUp;
+            set
+            {
+                _cleanUp = value;
+                OnPropertyChanged();
+                CleanUpError = !ValidateCleanUp() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateCleanUp()
+        {
+            return Validator.EmptyField(CleanUp);
+        }
+
+        public string CleanUpError
+        {
+            get => _cleanUpError;
+            set
+            {
+                _cleanUpError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Floor
+        {
+            get => _floor;
+            set
+            {
+                _floor = value;
+                OnPropertyChanged();
+                FloorError = !ValidateFloor() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateFloor()
+        {
+            return Validator.EmptyField(Floor);
+        }
+
+        public string FloorError
+        {
+            get => _floorError;
+            set
+            {
+                _floorError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string HotOrNot
+        {
+            get => _hotOrNot;
+            set
+            {
+                _hotOrNot = value;
+                OnPropertyChanged();
+                HotOrNotError = !ValidateHotOrNot() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateHotOrNot()
+        {
+            return Validator.EmptyField(HotOrNot);
+        }
+
+        public string HotOrNotError
+        {
+            get => _hotOrNotError;
+            set
+            {
+                _hotOrNotError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Music
+        {
+            get => _music;
+            set
+            {
+                _music = value;
+                OnPropertyChanged();
+                MusicError = !ValidateMusic() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateMusic()
+        {
+            return Validator.EmptyField(Music);
+        }
+
+        public string MusicError
+        {
+            get => _musicError;
+            set
+            {
+                _musicError = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SleepTime
+        {
+            get => _sleepTime;
+            set
+            {
+                _sleepTime = value;
+                OnPropertyChanged();
+                SleepTimeError = !ValidateSleepTime() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateSleepTime()
+        {
+            return Validator.EmptyField(SleepTime);
+        }
+
+        public string SleepTimeError
+        {
+            get => _sleepTimeError;
+            set
+            {
+                _sleepTimeError = value;
+                OnPropertyChanged();
+            }
+        }
+        public string WakeUpTime
+        {
+            get => _wakeUpTime;
+            set
+            {
+                _wakeUpTime = value;
+                OnPropertyChanged();
+                WakeUpTimeError = !ValidateWakeUpTime() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateWakeUpTime()
+        {
+            return Validator.EmptyField(WakeUpTime);
+        }
+
+        public string WakeUpTimeError
+        {
+            get => _wakeUpTimeError;
+            set
+            {
+                _wakeUpTimeError = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #endregion
     }
 }
--- SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/SpecificDataPage2ViewModel.cs
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/SpecificDataPage2ViewModel.cs
@@ -9,16 +9,11 @@ namespace SystemZarzadzaniaAkademikiem.ViewModels
         public readonly string index;
         private readonly User user;
         private readonly UserRepo userRepo;
+        public bool isValid = false;
 
         public SpecificDataPage2ViewModel(string index)
         {
             user = new User();
-            //Talkative = user?.Talkative;
-            //StudyField = user?.StudyField;
-            //Sporting = user?.Sporting;
-            //HomeBack = user?.HomeBack;
-            //Smoking = user?.Smoking;
-            //Party = user?.Party;
             this.index = index;
             SaveSpecificData2 = new Command(ExecuteSaveSpecificData2);
             userRepo = new UserRepo(App.Database);
@@ -35,14 +30,23 @@ namespace SystemZarzadzaniaAkademikiem.ViewModels
 
         private async void ExecuteSaveSpecificData2()
         {
-            var user = userRepo.GetUserAsync(index).Result;
-            user.Talkative = Talkative;
-            user.StudyField = StudyField;
-            user.Sporting = Sporting;
-            user.HomeBack = HomeBack;
-            user.Smoking = Smoking;
-            user.Party = Party;
-            await userRepo.SaveUserAsync(user);
+            isValid = Validate();
+            if (isValid)
+            {
+                var user = userRepo.GetUserAsync(index).Result;
+                user.Talkative = Talkative;
+                user.StudyField = StudyField;
+                user.Sporting = Sporting;
+                user.HomeBack = HomeBack;
+                user.Smoking = Smoking;
+                user.Party = Party;
+                await userRepo.SaveUserAsync(user);
+            }
+        }
+
+        public bool Validate()
+        {
+            return false;
         }
     }
 }
