ab51bf459016a15c5066168e02ac80dab521185b
Magda <bialaporcelana@gmail.com>
2019-06-02 19:56:11 +0200

    walidacja spec 2

--- SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/SpecificDataPage2ViewModel.cs
+++ SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/SystemZarzadzaniaAkademikiem/ViewModels/SpecificDataPage2ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using SystemZarzadzaniaAkademikiem.Models;
 using SystemZarzadzaniaAkademikiem.Services;
+using SystemZarzadzaniaAkademikiem.Validators;
 using Xamarin.Forms;
 
 namespace SystemZarzadzaniaAkademikiem.ViewModels
@@ -10,6 +11,19 @@ namespace SystemZarzadzaniaAkademikiem.ViewModels
         private readonly User user;
         private readonly UserRepo userRepo;
         public bool isValid = false;
+        private string _homeBack = "";
+        private string _bedLocationError = "";
+        private string _party = "";
+        private string _partyError = "";
+        private string _smoking = "";
+        private string _smokingError = "";
+        private string _sporting = "";
+        private string _sportingError = "";
+        private string _studyField = "";
+        private string _studyFieldError = "";
+        private string _talkative = "";
+        private string _talkativeError = "";
+
 
         public SpecificDataPage2ViewModel(string index)
         {
@@ -18,13 +32,6 @@ namespace SystemZarzadzaniaAkademikiem.ViewModels
             SaveSpecificData2 = new Command(ExecuteSaveSpecificData2);
             userRepo = new UserRepo(App.Database);
         }
-
-        public string HomeBack { get; set; }
-        public string Party { get; set; }
-        public string Smoking { get; set; }
-        public string Sporting { get; set; }
-        public string StudyField { get; set; } 
-        public string Talkative { get; set; }
 
         public Command SaveSpecificData2 { get; set; }
 
@@ -46,7 +53,165 @@ namespace SystemZarzadzaniaAkademikiem.ViewModels
 
         public bool Validate()
         {
-            return false;
+            return ValidateHomeBack() && ValidateTalkative() && ValidateSporting() && ValidateStudyField() &&
+                ValidateParty() && ValidateSmoking();
         }
+        #region
+        public string HomeBack
+        {
+            get => _homeBack;
+            set
+            {
+                _homeBack = value;
+                OnPropertyChanged();
+                HomeBackError = !ValidateHomeBack() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateHomeBack()
+        {
+            return Validator.EmptyField(HomeBack);
+        }
+
+        public string HomeBackError
+        {
+            get => _bedLocationError;
+            set
+            {
+                _bedLocationError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Talkative
+        {
+            get => _talkative;
+            set
+            {
+                _talkative = value;
+                OnPropertyChanged();
+                TalkativeError = !ValidateTalkative() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateTalkative()
+        {
+            return Validator.EmptyField(Talkative);
+        }
+
+        public string TalkativeError
+        {
+            get => _talkativeError;
+            set
+            {
+                _talkativeError = value;
+                OnPropertyChanged();
+            }
+        }
+
+       public string Sporting
+        {
+            get => _sporting;
+            set
+            {
+                _sporting = value;
+                OnPropertyChanged();
+                SportingError = !ValidateSporting() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateSporting()
+        {
+            return Validator.EmptyField(Sporting);
+        }
+
+        public string SportingError
+        {
+            get => _sportingError;
+            set
+            {
+                _sportingError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string StudyField
+        {
+            get => _studyField;
+            set
+            {
+                _studyField = value;
+                OnPropertyChanged();
+                StudyFieldError = !ValidateStudyField() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateStudyField()
+        {
+            return Validator.EmptyField(StudyField);
+        }
+
+        public string StudyFieldError
+        {
+            get => _studyFieldError;
+            set
+            {
+                _studyFieldError = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Party
+        {
+            get => _party;
+            set
+            {
+                _party = value;
+                OnPropertyChanged();
+                PartyError = !ValidateParty() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateParty()
+        {
+            return Validator.EmptyField(Party);
+        }
+
+        public string PartyError
+        {
+            get => _partyError;
+            set
+            {
+                _partyError = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Smoking
+        {
+            get => _smoking;
+            set
+            {
+                _smoking = value;
+                OnPropertyChanged();
+                SmokingError = !ValidateSmoking() ? "Pole nie może być puste" : "";
+            }
+        }
+
+        private bool ValidateSmoking()
+        {
+            return Validator.EmptyField(Smoking);
+        }
+
+        public string SmokingError
+        {
+            get => _smokingError;
+            set
+            {
+                _smokingError = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #endregion
     }
 }
